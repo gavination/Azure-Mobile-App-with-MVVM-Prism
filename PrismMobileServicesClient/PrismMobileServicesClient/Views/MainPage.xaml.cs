41e42b4003d4f4e12e2d229a814dbf5cd647854c
Gavin Bauman <gavinbauman1322@gmail.com>
2017-02-21 11:59:14 -0500

    Add project files.

---
+++ PrismMobileServicesClient/PrismMobileServicesClient/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace PrismMobileServicesClient.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
